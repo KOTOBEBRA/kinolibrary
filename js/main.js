81790cd8375a98f8af25f649395a6316f4c38ade
KOTOBEBRA <98683286+KOTOBEBRA@users.noreply.github.com>
2023-09-10 10:05:19 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let CINEMA_LIBRARY = {
+    "Barbie": {
+        "year":    2023,
+        "country": "USA",
+        "genre":   ["Comedy", "Adventure", "Fantasy"]
+    },
+
+    "Oppenheimer": {
+        "year":    2023,
+        "country": ["USA", "Great Britan"],
+        "genre":   ["Drama", "Biography Film", "Historical Film"]
+    },
+
+    "Spider-Man: Across the Spider-Verse": {
+        "year":    2023,
+        "country": "USA",
+        "genre":   ["Cartoon", "Superhero Film", "Action Movie", "Fiction"]
+    },
+};
